1d4995a893cad2dc82d8677c5250e29e97a4e920
Doppeltje <Doppeltje@users.noreply.github.com>
2020-06-02 17:25:14 +0200

    Backup 02-06

--- Ubuntopia/Assets/_Scripts/Player/Mounting.cs
+++ Ubuntopia/Assets/_Scripts/Player/Mounting.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class Mounting : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Mounting : MonoBehaviour {
+    [SerializeField] private GameObject playerObject;
+    [SerializeField] private Transform mountPosition;
+    private enum PlayerStatus { Grounded, Mounted, Flying }
+    private PlayerStatus status;
+    public Action PlayerHasMounted;
+
+
+    private void Start() {
+        status = PlayerStatus.Grounded;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            MountUp();
+        }
+    }
+
+    private void MountUp() {
+        Destroy(playerObject.GetComponent<Rigidbody>());
+        playerObject.transform.position = mountPosition.position;
+        playerObject.transform.SetParent(mountPosition);
+
+        status = PlayerStatus.Mounted;
+        PlayerHasMounted.Invoke();
+        print("Announced PlayerHasMounted");
     }
 }
