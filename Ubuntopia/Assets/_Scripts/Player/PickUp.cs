a5609e706e3780241b1cfff49c46d9e792d10cd5
blizzard190 <22256@ma-web.nl>
2020-06-03 11:14:51 +0200

    Basic pickup script + test scene

---
+++ Ubuntopia/Assets/_Scripts/Player/PickUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private int _found = 0;
+
+    public int total;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Artifact")
+        {
+            _found++;
+            //other.GetComponent<"Script with events">().play Voice line that 
+            Destroy(other.gameObject);
+            if (_found == total)
+            {
+                //Throw event for when you collect all artifacts;
+            }
+        }
+    }
+}
