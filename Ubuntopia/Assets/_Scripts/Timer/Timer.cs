357f4e672a4cdddbfb657e9673462ccdbb09810f
ColinvD <22950@ma-web.nl>
2020-06-04 14:39:09 +0200

    made a timer with a tester

---
+++ Ubuntopia/Assets/_Scripts/Timer/Timer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    private float startTime;
+    private float endTime;
+    private float totalTime;
+
+    // Either this script has to not destroy on load if going to a other scene or you need to give this value to a don't destroy script
+    public float TotalTime { get { return totalTime; } }
+
+    public void StartTimer()
+    {
+        startTime = Time.time;
+    }
+
+    public void EndTimer()
+    {
+        endTime = Time.time;
+        CalculateTotalTime();
+    }
+
+    private void CalculateTotalTime()
+    {
+        totalTime = endTime - startTime;
+    }
+}
