7c1e3a14964dce0e63c5ca7d24eb7a1630e966cf
ColinvD <22950@ma-web.nl>
2020-06-08 17:32:15 +0200

    added working controller input and sceneswitching

---
+++ Ubuntopia/Assets/_Scripts/SceneSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    [SerializeField] private int sceneIndex;
+    [SerializeField] private ControllerInput _controllerInput;
+
+    void Start()
+    {
+        _controllerInput.nextPhase += SwitchScenes;
+    }
+
+    private void SwitchScenes()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
---
+++ Ubuntopia/Assets/_Scripts/VisualLogger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VisualLogger : MonoBehaviour
+{
+    [SerializeField] private ControllerInput _controllerInput;
+    [SerializeField] private Text debugText;
+    
+    void Start()
+    {
+        _controllerInput.nextPhase += TestMessage;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void TestMessage()
+    {
+        debugText.text = "This works";
+    }
+}
