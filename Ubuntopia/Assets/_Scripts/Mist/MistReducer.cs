f9a189320f4496f404bac142d2d690b0755af5b7
blizzard190 <22256@ma-web.nl>
2020-06-11 13:08:10 +0200

    Mist unable script made

---
+++ Ubuntopia/Assets/_Scripts/Mist/MistReducer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UnityEngine;
+
+public class MistReducer : MonoBehaviour
+{
+    public List<ParticleSystem> mists = new List<ParticleSystem>();
+    
+    private bool _reducing;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            MistDown();
+    }
+
+    public void MistDown()
+    {
+        for (int i = 0; i < mists.Count; i++)
+        {
+            Debug.Log("Here");
+            var em = mists[i].emission;//.enabled = false;
+            em.enabled = false;
+        }
+    }
+}
