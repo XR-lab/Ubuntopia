a01f7494e852c4a4210320205c8b6b3d2fa5d871
DragstylerNL <dragstylernl@gmail.com>
2020-06-17 12:49:17 +0200

    rotation

--- Ubuntopia/Assets/_Scripts/Player/ManualFlight.cs
+++ Ubuntopia/Assets/_Scripts/Player/ManualFlight.cs
@@ -10,17 +10,21 @@ public class ManualFlight : MonoBehaviour
 	private bool _manualMovementActive = true;
 	private float _maxSpeed;
 	private Movement _movement;
+	private RotationFix _rotationFix;
+	private bool _outOffBoundarys = false;
 	
     // __________________________________________________________________________________________________________/ Start
     private void Start()
     {
 	    _movement = _player.GetComponent<Movement>();
 	    _maxSpeed = _movement.GetMaxSpeed();
+	    _rotationFix = GetComponent<RotationFix>();
     }	
 
     // _________________________________________________________________________________________________________/ Update
     private void Update()
     {
+	    if(_outOffBoundarys) return;
 	    _player.transform.position += _player.transform.forward * Time.deltaTime * _maxSpeed;
 	    if (Vector3.Distance(_rightHand.position, _leftHand.position) >= 0.6f)
 	    {
@@ -37,10 +41,17 @@ public class ManualFlight : MonoBehaviour
 	  //   }
     }
     
-    // _________________________________________________________________________________________________________/ Update
+    // ___________________________________________________________________________________________________________/ Move
     private void Move()
     {
-	    float difrence = _leftHand.position.y - _rightHand.position.y;
+	    float difrence = (_leftHand.position.y - _rightHand.position.y) / 2.5f;
 	    _player.Rotate(0, difrence, 0);
+	    _rotationFix.Rotate(difrence);
+    }
+    
+    // _______________________________________________________________________________________________/ SetBoundaryState
+    private void SetBoundaryState(bool state)
+    {
+	    _outOffBoundarys = state;
     }
 }
---
+++ Ubuntopia/Assets/_Scripts/RotationFix.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class RotationFix : MonoBehaviour
+{
+    // _________________________________________________________________________________________________________/ Rotate
+    public void Rotate(float difrence)
+    {
+        transform.Rotate(0, -difrence, 0);
+    }
+}
