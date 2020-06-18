3ea4d1fb99582e8880511dc534575c35c3775dac
Doppeltje <Doppeltje@users.noreply.github.com>
2020-06-18 12:45:30 +0200

    added sound events test scene

---
+++ Ubuntopia/Assets/_Scripts/SoundEvents/SFX_WingFlap.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFX_WingFlap : MonoBehaviour {
+    [SerializeField, Tooltip("Drag Audiomanager here.")]
+    private AudioManager _audioManager;
+
+    public void PlayWingFlap() {
+        print("playing wing flap");
+        _audioManager.Play("WingFlap_Test");
+    }
+}
