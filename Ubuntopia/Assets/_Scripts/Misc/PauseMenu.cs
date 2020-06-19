75fed802c90b49c81c2958e6b641e85024964c8d
blizzard190 <22256@ma-web.nl>
2020-06-19 14:55:41 +0200

    Made basic pause menu and script call

---
+++ Ubuntopia/Assets/_Scripts/Misc/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool _active = false;
+    
+    public List<GameObject> pauseObjects;
+
+    private void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.One) && !_active)
+            ActivePause();
+        else
+            DeactivePause();
+        
+    }
+
+    public void ActivePause()
+    {
+        Time.timeScale = 0;
+        for (int i = 0; i < pauseObjects.Count; i++)
+        {
+            pauseObjects[i].SetActive(true);
+        }
+    }
+
+    public void DeactivePause()
+    {
+        Time.timeScale = 1;
+        for (int i = 0; i < pauseObjects.Count; i++)
+        {
+            pauseObjects[i].SetActive(false);
+        }
+    }
+}
