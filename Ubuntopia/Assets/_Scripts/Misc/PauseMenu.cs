46bd309aaf3cc0ecb69b2d76562609b7cc5ec998
blizzard190 <22256@ma-web.nl>
2020-06-23 14:21:02 +0200

    fixed posistion and rotation of the UI in world space

--- Ubuntopia/Assets/_Scripts/Misc/PauseMenu.cs
+++ Ubuntopia/Assets/_Scripts/Misc/PauseMenu.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour
 {
     private bool _active = false;
+    [SerializeField] private GameObject _player;
     
     public List<GameObject> pauseObjects;
+    public int offset;
 
     private void Update()
     {
-        if(OVRInput.GetDown(OVRInput.Button.One) && !_active)
+        if(OVRInput.GetDown(OVRInput.Button.Two) && !_active|| Input.GetKeyDown(KeyCode.A))
             ActivePause();
-        else
+        else if(OVRInput.GetDown(OVRInput.Button.Two) && _active)
             DeactivePause();
         
     }
@@ -21,8 +23,13 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 0;
         for (int i = 0; i < pauseObjects.Count; i++)
         {
-            pauseObjects[i].SetActive(true);
+            var obj = pauseObjects[i];
+            obj.transform.position = _player.transform.position + _player.transform.forward * offset;
+            obj.transform.rotation = Quaternion.Euler(obj.transform.eulerAngles.x, _player.transform.eulerAngles.y, obj.transform.eulerAngles.z); //= new Quaternion(obj.transform.rotation.x, _player.transform.rotation.y, obj.transform.rotation.z, 1);
+            obj.SetActive(true);
         }
+
+        _active = true;
     }
 
     public void DeactivePause()
@@ -32,5 +39,7 @@ public class PauseMenu : MonoBehaviour
         {
             pauseObjects[i].SetActive(false);
         }
+
+        _active = false;
     }
 }
