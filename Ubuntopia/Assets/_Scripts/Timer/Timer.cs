e48164e2f03b828cc1ae6005babf9a6fb72f3514
ColinvD <22950@ma-web.nl>
2020-06-23 13:03:17 +0200

    changed head collider

--- Ubuntopia/Assets/_Scripts/Timer/Timer.cs
+++ Ubuntopia/Assets/_Scripts/Timer/Timer.cs
@@ -5,12 +5,18 @@ public class Timer : MonoBehaviour
     private float startTime;
     private float endTime;
     private float totalTime;
+    
+    private float endSeconds;
+    private float endMinutes;
 
     // Either this script has to not destroy on load if going to a other scene or you need to give this value to a don't destroy script
     public float TotalTime { get { return totalTime; } }
+    public float EndSeconds { get { return endSeconds; } }
+    public float EndMinutes { get { return endMinutes; } }
 
     public void StartTimer()
     {
+        DontDestroyOnLoad(gameObject);
         startTime = Time.time;
     }
 
@@ -23,5 +29,10 @@ public class Timer : MonoBehaviour
     private void CalculateTotalTime()
     {
         totalTime = endTime - startTime;
+    }
+
+    private void FormatTime()
+    {
+        
     }
 }
