171292b001655767a0088def6d944b7c3300a661
Doppeltje <Doppeltje@users.noreply.github.com>
2020-06-24 09:55:24 +0200

    first and backup commit. Stashing, because switching to sound events for abit.

---
+++ Ubuntopia/Assets/_Scripts/LevelLoader/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour {
+    [SerializeField] private WaypointTileData _data;
+
+    [SerializeField] private List<GameObject> waypoints;
+    [SerializeField] private List<GameObject> tiles;
+    [SerializeField] private List<bool> active;
+    
+    private Dictionary<GameObject, bool> tileActivation = new Dictionary<GameObject, bool>();
+
+    private void Start() {
+        InitializeDictionaries();
+    }
+
+    private void InitializeDictionaries() {
+        foreach (var obj in _data.waypointTileData) {
+            waypoints.Add(obj.waypoint);
+            tiles.Add(obj.tile);
+            active.Add(obj.activate);
+        }
+        
+        // InitializeTiles();
+    }
+
+    // private void InitializeTiles() {
+    //     for (int i = 0; i < tiles.Count; i++) {
+    //         tiles[i].SetActive(tileActivation[active[i]]);
+    //     }
+    // }
+}
---
+++ Ubuntopia/Assets/_Scripts/LevelLoader/WaypointTileLink.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointTileLink : ScriptableObject {
+    public string linkName;
+    public GameObject waypoint;
+    public GameObject tile;
+    public bool activate = false;
+}
