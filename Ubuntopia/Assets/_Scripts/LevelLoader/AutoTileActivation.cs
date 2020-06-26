10d80716d21210b55a14d7c3b206300e85340a0a
Doppeltje <Doppeltje@users.noreply.github.com>
2020-06-26 11:45:50 +0200

    updated tileactivation script. now correctly turns meshrenderers on and off. changed filenames of files no longer in use.

--- Ubuntopia/Assets/_Scripts/LevelLoader/AutoTileActivation.cs
+++ Ubuntopia/Assets/_Scripts/LevelLoader/AutoTileActivation.cs
@@ -14,6 +14,9 @@ public class AutoTileActivation : MonoBehaviour {
 
     [SerializeField, Tooltip("How many tiles should be visable in front the player.")]
     private int limit = 3;
+
+    [SerializeField, Tooltip("How many tiles to show on load.")]
+    private int onLoad = 4;
     
     private List<GameObject> waypoints = new List<GameObject>();
     private List<GameObject> tiles = new List<GameObject>();
@@ -28,10 +31,15 @@ public class AutoTileActivation : MonoBehaviour {
        InitializeWaypointList();
        InitializeTilesList();
        LinkWaypointWithTiles();
-       DeactivateAfterLink();
+       LoadTilesOnStart();
 
        // Subscribe to event.
        _waypoints.Arrived += SetActiveStateForTile;
+    }
+
+    // Unsubscribe from event on destroy.
+    private void OnDestroy() {
+        _waypoints.Arrived -= SetActiveStateForTile;
     }
 
     private void InitializeWaypointList() {
@@ -63,15 +71,15 @@ public class AutoTileActivation : MonoBehaviour {
             }
         }
     }
-
-    // Deactivate after linking waypoints and tiles.
-    private void DeactivateAfterLink() {
-        for (int i = limit; i < tiles.Count; i++) {
-            tileActiveMap[tiles[i]] = false;
-            tiles[i].SetActive(tileActiveMap[tiles[i]]);
+    
+    // Load x amount of tiles at the start of the game.
+    private void LoadTilesOnStart() {
+        for (int i = 0; i < onLoad; i++) {
+            tiles[i].GetComponent<MeshRenderer>().enabled = true;
         }
     }
 
+    // If player reached waypoint, (de)activate tiles according to the waypoint position.
     private void SetActiveStateForTile(GameObject obj) {
         if (waypointTileMap.ContainsKey(obj)) {
             for (int i = 0; i < tiles.Count; i++) {
@@ -80,32 +88,79 @@ public class AutoTileActivation : MonoBehaviour {
                     // Set new limit.
                     int newLimit = limit;
                     newLimit += i + 2;
-                    tileActiveMap[tiles[i]] = true;
-                    tiles[i].SetActive(tileActiveMap[tiles[i]]);
 
                     // Activate tiles around player.
                     for (int l = i; l < newLimit; l++) {
                         if (l < tiles.Count) {
                             // In front of the player.
-                            tileActiveMap[tiles[l]] = true;
-                            tiles[l].SetActive(tileActiveMap[tiles[l]]);
+                            tiles[l].GetComponent<MeshRenderer>().enabled = true;
                             
                             // Behind the player.
+                            // Create new variable based on current limit, to calculate tile integer behind the player.
                             int b = l - limit;
                             if (b >= 0) {
-                                tileActiveMap[tiles[b]] = true;
-                                tiles[b].SetActive(tileActiveMap[tiles[b]]);
+                                tiles[b].GetComponent<MeshRenderer>().enabled = true;
                             }
                         }
                     }
-                    
+                    // Exit function.
                     return;
                 }
                 
-                // Deactivate tiles behind us.
-                tileActiveMap[tiles[i]] = false;
-                tiles[i].SetActive(tileActiveMap[tiles[i]]);
+                // Deactivate tiles until reaching the back-limit.
+                tiles[i].GetComponent<MeshRenderer>().enabled = false;
             }
         }
     }
+    
+    
+    // Backup code.
+    
+    // Not used at the moment.
+    // Deactivate after linking waypoints and tiles.
+    // private void DeactivateAfterLink() {
+    //     for (int i = limit; i < tiles.Count; i++) {
+    //         tileActiveMap[tiles[i]] = false;
+    //         tiles[i].SetActive(tileActiveMap[tiles[i]]);
+    //     }
+    // }
+    // 
+    // // (De)Activates gameobjects rather than dis/enables meshrenderers.
+    // // If player reached waypoint, (de)activate tiles according to the waypoint position.
+    // private void SetActiveStateForTile(GameObject obj) {
+    //     if (waypointTileMap.ContainsKey(obj)) {
+    //         for (int i = 0; i < tiles.Count; i++) {
+    //             // If tiles[i] is connected to our current waypoint, stop deactivating.
+    //             if (tiles[i] == waypointTileMap[obj]) {
+    //                 // Set new limit.
+    //                 int newLimit = limit;
+    //                 newLimit += i + 2;
+    //                 tileActiveMap[tiles[i]] = true;
+    //                 tiles[i].SetActive(tileActiveMap[tiles[i]]);
+    //
+    //                 // Activate tiles around player.
+    //                 for (int l = i; l < newLimit; l++) {
+    //                     if (l < tiles.Count) {
+    //                         // In front of the player.
+    //                         tileActiveMap[tiles[l]] = true;
+    //                         tiles[l].SetActive(tileActiveMap[tiles[l]]);
+    //                         
+    //                         // Behind the player.
+    //                         int b = l - limit;
+    //                         if (b >= 0) {
+    //                             tileActiveMap[tiles[b]] = true;
+    //                             tiles[b].SetActive(tileActiveMap[tiles[b]]);
+    //                         }
+    //                     }
+    //                 }
+    //                 
+    //                 return;
+    //             }
+    //             
+    //             // Deactivate tiles behind us.
+    //             tileActiveMap[tiles[i]] = false;
+    //             tiles[i].SetActive(tileActiveMap[tiles[i]]);
+    //         }
+    //     }
+    // }
 }
