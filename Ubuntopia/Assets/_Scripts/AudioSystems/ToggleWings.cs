aa279b536dd6b59bba5280b26607e6bbe3b324d2
Doppeltje <Doppeltje@users.noreply.github.com>
2020-06-29 11:46:23 +0200

    implemented wings animation. Added functionality for animation on/off toggle.

---
+++ Ubuntopia/Assets/_Scripts/AudioSystems/ToggleWings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class ToggleWings : MonoBehaviour {
+    [SerializeField, Tooltip("Drag manual flying wings here (from scene hierarchy).")]
+    private GameObject obj1;
+
+    [SerializeField, Tooltip("Drag automatic flying wings here (from scene hierarchy).")]
+    private GameObject obj2;
+
+    [SerializeField, Tooltip("Manual fly animation yes/no.")]
+    private bool manual = false;
+
+    private void Start() {
+        ToggleObject(manual);
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            Toggle();
+            ToggleObject(manual);
+        }
+    }
+
+    public void Toggle() {
+        manual = !manual;
+    }
+
+    private void ToggleObject(bool b) {
+        obj1.SetActive(b);
+        obj2.SetActive(!b);
+    }
+}
