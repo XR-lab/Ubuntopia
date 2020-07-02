8ed0e16f61ec3123982376baedf8c1ce7ea0c87d
Doppeltje <Doppeltje@users.noreply.github.com>
2020-07-02 11:24:45 +0200

    adjusted sound volumes. added artifact audio source spatial zones. added artifact pickup sfx. disabled mesh renderers waypoints. adjusted hierarchy location of trees to be child of first tile.

--- Ubuntopia/Assets/_Scripts/Player/Collect.cs
+++ Ubuntopia/Assets/_Scripts/Player/Collect.cs
@@ -1,13 +1,14 @@
 ﻿using UnityEngine;
 
-public class Collect : MonoBehaviour
-{
+public class Collect : MonoBehaviour {
     [SerializeField] private MistReducer _mist;
+    private GameObject artifactMist;
     
-    public void Collected()
-    {
-        //Enter follow uo code on collection here(Cutscene/other code)
+    public void Collected() {
+        // Enter follow uo code on collection here(Cutscene/other code).
         _mist.MistDown();
+        artifactMist = _mist.gameObject;
+        artifactMist.GetComponent<AudioSource>().Stop();
         GameObject.Destroy(this.gameObject);
     }
 }
