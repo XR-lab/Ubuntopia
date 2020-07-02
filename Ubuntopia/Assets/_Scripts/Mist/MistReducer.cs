aa92465174dfde1cdcd365bb50a06cc8c6739dc3
Doppeltje <Doppeltje@users.noreply.github.com>
2020-07-02 11:56:37 +0200

    adjusted artifact volume to decrease over time rather than stopping. cleaned up code.

--- Ubuntopia/Assets/_Scripts/Mist/MistReducer.cs
+++ Ubuntopia/Assets/_Scripts/Mist/MistReducer.cs
@@ -4,23 +4,55 @@ public class MistReducer : MonoBehaviour
 {
     private ParticleSystem mist;
     private bool _reducing;
+    private AudioSource source;
+    private bool startCounting = false;
+    private float counter = 0f;
+
+    private float stopTime = 0;
     [SerializeField]private float mistReducingSpeed = 0.7f;
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
             MistDown();
+
+        if (startCounting) {
+            StopArtifactSFX();
+        }
     }
     private void Start()
     {
         mist = this.GetComponent<ParticleSystem>();
+        source = GetComponent<AudioSource>();
     }
 
-    public void MistDown()
-    {
+    // Called when artifact is picked up.
+    public void MistDown() {
         var em = mist.emission;
         var main = mist.main;
         em.enabled = false;
         main.simulationSpeed = mistReducingSpeed;
+        // Activate counter.
+        startCounting = true;
+    }
+
+    private void StopArtifactSFX() {
+        // If object doesn't have audio source, stop function.
+        if (!source) {
+            startCounting = false;
+            return;
+        }
+        
+        counter += Time.deltaTime;
+        if (counter >= stopTime) {
+            if (source.volume > 0) {
+                source.volume -= 1 * Time.deltaTime;
+            }
+            else {
+                // Stop and destroy source, since it's no longer needed.
+                source.Stop();
+                Destroy(source);
+            }
+        }
     }
 }
--- Ubuntopia/Assets/_Scripts/Player/Collect.cs
+++ Ubuntopia/Assets/_Scripts/Player/Collect.cs
@@ -2,13 +2,10 @@
 
 public class Collect : MonoBehaviour {
     [SerializeField] private MistReducer _mist;
-    private GameObject artifactMist;
-    
+
     public void Collected() {
         // Enter follow uo code on collection here(Cutscene/other code).
         _mist.MistDown();
-        artifactMist = _mist.gameObject;
-        artifactMist.GetComponent<AudioSource>().Stop();
         GameObject.Destroy(this.gameObject);
     }
 }
