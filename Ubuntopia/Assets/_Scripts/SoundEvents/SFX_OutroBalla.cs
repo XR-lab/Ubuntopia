502d9ac13edca2726a8cff0962d5432a91471b65
Doppeltje <Doppeltje@users.noreply.github.com>
2020-07-07 11:27:31 +0200

    created sfx script outro

---
+++ Ubuntopia/Assets/_Scripts/SoundEvents/SFX_OutroBalla.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+public class SFX_OutroBalla : MonoBehaviour {
+    private string name = "SFX_OutroBalla_";
+    
+    // References.
+    private AudioManager _audioManager = AudioManager.instance;
+
+    private void PlaySFX() {
+        string newName = name;
+        
+        // Get language from audio manager.
+        Enum language = _audioManager.GetLanguage();
+        
+        // Switch.
+        switch (language) {
+            case language.English:
+                newName += "ENG";
+                break;
+            case language.Dutch:
+                newName += "DUT";
+                break;
+        }
+
+        // What is new name?
+        print(newName);
+        
+        // Play sfx.
+        _audioManager.Play(newName);
+    }
+}
