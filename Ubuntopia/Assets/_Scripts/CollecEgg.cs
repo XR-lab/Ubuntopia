c38d8514322158f266d2aa8d89fae83f93687b47
DragstylerNL <dragstylernl@gmail.com>
2020-07-07 11:19:40 +0200

    Update CollecEgg.cs

--- Ubuntopia/Assets/_Scripts/CollecEgg.cs
+++ Ubuntopia/Assets/_Scripts/CollecEgg.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CollecEgg : MonoBehaviour
 {
     
     public void Collected() {
-        GameObject.Destroy(this.gameObject);
+        Destroy(gameObject);
     }
 }
 
