1323c89455edc88626d35aad884d61945887d787
wmartijn60 <34513939+wmartijn60@users.noreply.github.com>
2020-07-07 11:11:23 +0200

    EasterEggAdded

---
+++ Ubuntopia/Assets/_Scripts/CollecEgg.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollecEgg : MonoBehaviour
+{
+    
+    public void Collected() {
+        GameObject.Destroy(this.gameObject);
+    }
+}
+
+
