a547a47432aeda0d0cc29d7f4cf4acd0f06f2a6e
DragstylerNL <dragstylernl@gmail.com>
2020-07-09 08:57:14 +0200

    hmyes

--- Ubuntopia/Assets/_Scripts/Gameplay/StartOutroSFX.cs
+++ Ubuntopia/Assets/_Scripts/Gameplay/StartOutroSFX.cs
@@ -3,13 +3,9 @@ using UnityEngine;
 
 public class StartOutroSFX : MonoBehaviour {
     // References.
-    [SerializeField, Header("Drag AudioManager here (from scene hierarchy).")]
+    [SerializeField, Header("Drag SFX here (from scene hierarchy).")]
     private SFX_OutroBalla _sfx;
     
-    // References.
-    [SerializeField, Header("Drag AudioManager here (from scene hierarchy).")]
-    private AudioManager _audioManager;
-
     private void Start() {
         // When entering OutroScene, start outro sfx.
         _sfx.PlaySFX();
@@ -20,7 +16,7 @@ public class StartOutroSFX : MonoBehaviour {
 
     private IEnumerator StopAmbient() {
         yield return new WaitForEndOfFrame();
-        _audioManager.Stop("AmbientMusic0");
-        _audioManager.Stop("AmbientMusic1");
+        AudioManager.instance.Stop("AmbientMusic0");
+        AudioManager.instance.Stop("AmbientMusic1");
     }
 }
--- Ubuntopia/Assets/_Scripts/SoundEvents/SFX_OutroBalla.cs
+++ Ubuntopia/Assets/_Scripts/SoundEvents/SFX_OutroBalla.cs
@@ -5,11 +5,7 @@ public class SFX_OutroBalla : MonoBehaviour {
     // Default string name for sfx.
     private string name = "Outro_Balla_";
     
-    // References.
-    [SerializeField, Header("Drag AudioManager here.")]
-    private AudioManager _audioManager;
-
     public void PlaySFX() {
-        _audioManager.PlayLanguage(name);
+        AudioManager.instance.PlayLanguage(name);
     }
 }
